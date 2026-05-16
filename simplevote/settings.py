cb4927c46dd5396059dafe89ff91640759c3f81a
AbidambeFavour <abidambefavour2@gmail.com>
2026-05-16 17:24:15 +0100

    Add WhiteNoise middleware and static files storage for Render

--- simplevote/settings.py
+++ simplevote/settings.py
@@ -85,6 +85,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -180,6 +181,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.WhiteNoiseStaticFilesStorage'
 
 # Custom user model
 AUTH_USER_MODEL = 'accounts.User'
